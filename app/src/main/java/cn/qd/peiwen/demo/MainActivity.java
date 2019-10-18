f1f7cb48d63150948ea94b124772c0a582e08af2
peiwen.liu <peiwen.liu@picovr.com>
2019-10-18 09:43:08 +0800

    Init the latest PWLogger 1.0.3

--- app/src/main/java/cn/qd/peiwen/demo/MainActivity.java
+++ app/src/main/java/cn/qd/peiwen/demo/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity implements FingerPrintListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PWLogger.configure(this);
         RFIDReaderManager.getInstance().init(this);
         FingerPrintManager.getInstance().init(this);
     }

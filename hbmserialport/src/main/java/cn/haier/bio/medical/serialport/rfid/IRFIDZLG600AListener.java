a1a6ffdb9a2c8549c5686ac0160b8ba4aa7400b8
bang.liu <bang.liu@haierbiomedical.com>
2020-06-28 10:02:56 +0800

    Remove demos

--- hbmserialport/src/main/java/cn/haier/bio/medical/serialport/rfid/IRFIDZLG600AListener.java
+++
@@ -1,8 +0,0 @@
-package cn.haier.bio.medical.serialport.rfid;
-
-public interface IRFIDZLG600AListener {
-    void onRFIDZLG600AReady();
-    void onRFIDZLG600AConnected();
-    void onRFIDZLG600AException();
-    void onRFIDZLG600ARecognized(long id, String card);
-}
--- hbmserialport/src/main/java/cn/haier/bio/medical/serialport/rsms/entity/send/IRSMSSendEntity.java
+++
@@ -1,5 +0,0 @@
-package cn.haier.bio.medical.serialport.rsms.entity.send;
-
-public interface IRSMSSendEntity {
-    byte[] packageSendMessage();
-}
